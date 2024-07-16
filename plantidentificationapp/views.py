b6d4be5aac059e4c826c7c109d0e022cc65e6271
Nicholas Cutlip <cutlipn@gmail.com>
2024-07-15 17:51:00 -0700

    Define API view classes.

--- plantidentificationapp/views.py
+++ plantidentificationapp/views.py
@@ -7,4 +7,31 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SerializerAPI1, SerializerAPI2
 
-# Create your views here.
+# Define API view classes
+class API1View(APIView):
+    def get(self, request):
+        data = [
+            {"name": "Item 1", "value": 10},
+            {"name": "Item 2", "value": 20},
+        ]
+        return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SerializerAPI1(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class API2View(APIView):
+    def get(self, request):
+        data = [
+            {"name": "Item 1", "value": 10},
+            {"name": "Item 2", "value": 20},
+        ]
+        return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SerializerAPI2(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
