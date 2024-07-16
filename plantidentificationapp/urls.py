3870e6effa4e7f51c25cf10c0fe3fc5e637cc462
Nicholas Cutlip <cutlipn@gmail.com>
2024-07-15 17:57:39 -0700

    Add imports, define urls in urls.py

--- plantidentificationapp/urls.py
+++ plantidentificationapp/urls.py
@@ -1,1 +1,8 @@
- 
+from django.urls import path
+from .views import API1View, API2View
+
+# Define API URL patterns
+urlpatterns = [
+    path('api/1/', API1View.as_view(), name='API1'),
+    path('api/2/', API2View.as_view(), name='API2')
+]
