983831f323e93e68104054ec7c7e239f6b6fdb09
Nicholas Cutlip <cutlipn@gmail.com>
2024-07-15 17:46:36 -0700

    Define serializer classes.

--- plantidentificationapp/serializers.py
+++ plantidentificationapp/serializers.py
@@ -1,3 +1,12 @@
- from rest_framework import serializers
+from rest_framework import serializers
 
+# Although we are not using models as we have no database,
+# serializers are still useful for data validation and transformation
+class SerializerAPI1(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    value = serializers.IntegerField()
+
+class SerializerAPI2(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    value = serializers.IntegerField()
  
