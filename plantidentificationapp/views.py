a7e0d670cc36872ae04e36fa4a3b35890f99505a
Nicholas Cutlip <cutlipn@gmail.com>
2024-07-15 17:48:52 -0700

    Add imports to views.py

--- plantidentificationapp/views.py
+++ plantidentificationapp/views.py
@@ -1,3 +1,10 @@
+# Define views to validate and transform incoming data,
+# and to prepare the outgoing data.
+
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SerializerAPI1, SerializerAPI2
 
 # Create your views here.
